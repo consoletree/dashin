1ece94ad8156fe652c7084c2321478b460dc8024
consoletree <76278342+consoletree@users.noreply.github.com>
2026-02-05 08:38:37 -0500

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_BASE = '/api/v1';
+const API_BASE = import.meta.env.VITE_API_URL || '/api/v1';
 
 const api = axios.create({
-  baseURL: API_BASE,
+  baseURL: API_BASE + '/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
